e280b222e8cd78ec0d67fd4a90b20c52b2d125b7
christophervalles <info@christophervalles.com>
2011-07-25 16:48:08 +0200

    Refactorized plugin using boilerplate as a base

--- js/jquery.location.plugin.js
+++ js/jquery.location.plugin.js
@@ -1,133 +1,154 @@
-(function($){
-    //Main function
-    $.fn.locationWidget = function(options){
-        $.fn.locationWidget.options = $.extend({}, $.fn.locationWidget.options, options);
-        options = $.fn.locationWidget.options;
-        //Prepare the hidden fields
-        var cityWoeidField = $('<input></input>').attr('type', 'hidden').attr('name', 'city_woeid').attr('id', 'city_woeid');
-        var countryIsoField = $('<input></input>').attr('type', 'hidden').attr('name', 'country_iso').attr('id', 'country_iso');
-        $(options.searchInput).after(cityWoeidField);
-        $(options.searchInput).after(countryIsoField);
-        
-        //Prepare the UI
-        var div = $('<div></div>').addClass('location-results').attr('id', 'location-results');
-        var header = $('<p></p>').html('Choose the correct location below...');
-        var ul = $('<ul></ul>').attr('id', 'location-suggest');
-        $(options.searchInput).parent().append(div.append(header).append(ul));
-        $(options.resultCanvas).hide();
-        
-        //Avoid submit the form when enter key pressed
-        if($(options.searchInput).length > 0){
-            $(options.searchInput).bind('keypress', function(event){
-                if(event.which == 13){
-                    event.preventDefault();
-                    if($(options.searchInput).val().length > 0){
-                        $.fn.locationWidget.geocode();
-                    }
-                }
-            });
+(function($) {
+    $.locationWidget = function(element, options) {
+        var defaults = {
+            resultCanvas: '#location-results',
+            resultCanvasId: 'location-results',
+            resultList: '#location-suggest',
+            resultListId: 'location-suggest',
+            searchInput: '',
+            searchButton: '',
+            yahooApiKey: '',
+            jsonDataField: '#location_data',
+            jsonDataFieldId: 'location_data',
+            locale: 'en'
         }
-        
-        //Bind the search
-        if($(options.searchButton).length > 0){
-            $(options.searchButton).bind('click', function(e){
-                if($(options.cityWoeid).val().length == 0 && $(options.searchInput).val().length > 0){
-                    $.fn.locationWidget.geocode();
-                }else{
-                    if($(options.searchInput).hasClass('edited') && $(options.searchInput).val().length > 0){
-                        $.fn.locationWidget.geocode();
-                    }
-                }
-            });
+
+        var texts = {
+            resultsHint: 'Choose the correct location below...'
         }
-        
-        //Bind the blur event
-        if($(options.searchInput).length > 0){
-            $(options.searchInput).bind('blur', function(){
-                if($(options.cityWoeid).val().length == 0 && $(options.searchInput).val().length > 0){
-                    $.fn.locationWidget.geocode();
-                }else{
-                    if($(options.searchInput).hasClass('edited') && $(options.searchInput).val().length > 0){
-                        $.fn.locationWidget.geocode();
-                    }
-                }
-            });
+
+        var searchInProgress = false;
+
+        var plugin = this;
+
+        plugin.settings = {}
+        plugin.translations = {}
+
+        var $element = $(element),
+             element = element;
+
+        plugin.init = function() {
+            plugin.settings = $.extend({}, defaults, options);
+            plugin.translations = $.extend({}, texts, options);
             
-            $(options.searchInput).bind('change', function(){
-                $(options.searchInput).addClass('edited');
-            });
-        }
-    };
-    
-    //Geocode location with yahoo api
-    $.fn.locationWidget.geocode = function(){
-        if($.fn.locationWidget.searchInProgess){
-            return false;
-        }
-        
-        options = $.fn.locationWidget.options;
-        
-        $(options.searchInput).css('background', '#fff url(images/spinner.gif) 98% 50% no-repeat');
-        
-        var url = 'http://where.yahooapis.com/v1/places$and(.q(' + escape($(options.searchInput).val()) + '),.type(7));count=0?format=json&lang=' + options.locale + '&appid=' + options.yahooApiKey;
-        
-        $.getJSON(url, function(data){
-            if(data.places.total > 0){
-                $(options.searchInput).attr('readonly', 'readonly');
-                
-                $(options.resultList).html('');
-                $(data.places.place).each(function(i, e){
-                    var name = e.name;
-                    if(e.admin1 != e.name){
-                        name += ', ' + e.admin1;
-                    }
-                    
-                    name += ', ' + e.country;
-                    
-                    $(options.resultList).append($('<li></li>').append($('<a></a>').attr('href' , '#').html(name)).bind('click', function(event){
+            //Register the templates
+            $.template('noResults', 'No results for <strong>${SearchTerm}</strong>');
+
+            //Prepare the hidden field
+            var jsonField = $('<input></input>').attr('type', 'hidden').attr('name', plugin.settings.jsonDataFieldId).attr('id', plugin.settings.jsonDataFieldId);
+            $(plugin.settings.searchInput).after(jsonField);
+            
+            //Prepare the UI
+            var div = $('<div></div>').addClass('location-results').attr('id', plugin.settings.resultCanvasId);
+            var header = $('<p></p>').html(plugin.translations.resultsHint);
+            var ul = $('<ul></ul>').attr('id', plugin.settings.resultListId);
+            $(plugin.settings.searchInput).parent().append(div.append(header).append(ul));
+            $(plugin.settings.resultCanvas).hide();
+            
+            //Avoid submit the form when enter key pressed
+            if($(plugin.settings.searchInput).length > 0){
+                $(plugin.settings.searchInput).bind('keypress', function(event){
+                    if(event.which == 13){
                         event.preventDefault();
-                        $(options.cityWoeid).val(e.woeid);
-                        $(options.countryIso).val(e['country attrs'].code);
-                        $(options.searchInput).val($(this).children().html());
-                        $(options.resultCanvas).hide();
-                        $(options.searchInput).removeClass('edited');
-                        
-                        $(options.searchInput).attr('readonly', '');
-                        $(options.searchInput).blur();
-                        
-                        $.fn.locationWidget.searchInProgess = false;
-                    }));
+                        if($(plugin.settings.searchInput).val().length > 0){
+                            geocode();
+                        }
+                    }
                 });
-                
-                $(options.resultCanvas + ' p').html('Choose the correct location below...').removeClass('noresults');
-                $(options.resultList).show();
-                
-                $.fn.locationWidget.searchInProgess = true;
-            }else{
-                $(options.resultCanvas + ' p').html('No results for <strong>' + $(options.searchInput).val() + '</strong>').addClass('noresults');
-                $(options.resultList).hide();
-                
-                $(options.searchInput).removeClass('edited');
-                
-                $.fn.locationWidget.searchInProgess = false;
             }
             
-            $(options.searchInput).css('background', '#FFF');
-            $(options.resultCanvas).show();
-        });
-    };
-    
-    $.fn.locationWidget.searchInProgess = false;
-    
-    //Default options
-    $.fn.locationWidget.options = {
-        resultCanvas: '#location-results',
-        resultList: '#location-suggest',
-        searchInput: '',
-        searchButton: '',
-        yahooApiKey: '',
-        cityWoeid: '#city_woeid',
-        countryIso: '#country_iso',
-        locale: 'en'
+            //Bind the search
+            if($(plugin.settings.searchButton).length > 0){
+                $(plugin.settings.searchButton).bind('click', function(e){
+                    if($(plugin.settings.jsonDataField).val().length == 0 && $(plugin.settings.searchInput).val().length > 0){
+                        geocode();
+                    }else{
+                        if($(plugin.settings.searchInput).hasClass('edited') && $(plugin.settings.searchInput).val().length > 0){
+                            geocode();
+                        }
+                    }
+                });
+            }
+            
+            //Bind the blur event
+            if($(plugin.settings.searchInput).length > 0){
+                $(plugin.settings.searchInput).bind('blur', function(){
+                    if($(plugin.settings.jsonDataField).val().length == 0 && $(plugin.settings.searchInput).val().length > 0){
+                        geocode();
+                    }else{
+                        if($(plugin.settings.searchInput).hasClass('edited') && $(plugin.settings.searchInput).val().length > 0){
+                            geocode();
+                        }
+                    }
+                });
+                
+                $(plugin.settings.searchInput).bind('change', function(){
+                    $(plugin.settings.searchInput).addClass('edited');
+                });
+            }
+        }
+
+        var geocode = function() {
+            if(plugin.searchInProgess){
+                return false;
+            }
+            
+            $(plugin.settings.searchInput).css('background', '#fff url(images/spinner.gif) 98% 50% no-repeat');
+            
+            var url = 'http://where.yahooapis.com/v1/places$and(.q(' + escape($(plugin.settings.searchInput).val()) + '),.type(7));count=0?format=json&lang=' + plugin.settings.locale + '&appid=' + plugin.settings.yahooApiKey;
+
+            $.getJSON(url, function(data){
+                if(data.places.total > 0){
+                    $(plugin.settings.searchInput).attr('readonly', 'readonly');
+                    
+                    $(plugin.settings.resultList).html('');
+                    $(data.places.place).each(function(i, e){
+                        var name = e.name;
+                        if(e.admin1 != e.name){
+                            name += ', ' + e.admin1;
+                        }
+                        
+                        name += ', ' + e.country;
+                        
+                        $(plugin.settings.resultList).append($('<li></li>').append($('<a></a>').attr('href' , '#').html(name)).bind('click', function(event){
+                            event.preventDefault();
+                            $(plugin.settings.jsonDataField).val(JSON.stringify(e));
+                            $(plugin.settings.searchInput).val($(this).children().html());
+                            $(plugin.settings.searchInput).removeClass('edited');
+                            $(plugin.settings.searchInput).removeAttr('readonly');
+                            $(plugin.settings.searchInput).blur();
+
+                            $(plugin.settings.resultCanvas).hide();
+                            
+                            plugin.searchInProgess = false;
+                        }));
+                    });
+                    
+                    $(plugin.settings.resultCanvas + ' p').html(plugin.translations.resultsHint).removeClass('noresults');
+                    $(plugin.settings.resultList).show();
+                    
+                    plugin.searchInProgess = true;
+                }else{
+                    $(plugin.settings.resultCanvas + ' p').html($.tmpl('noResults', {SearchTerm: $(plugin.settings.searchInput).val()} )).addClass('noresults');
+                    $(plugin.settings.resultList).hide();
+                    
+                    $(plugin.settings.searchInput).removeClass('edited');
+                    
+                    plugin.searchInProgess = false;
+                }
+                
+                $(plugin.settings.searchInput).removeAttr('style');
+                $(plugin.settings.resultCanvas).show();
+            });
+        }
+
+        plugin.init();
     }
-})(jQuery);
+
+    $.fn.locationWidget = function(options) {
+        if (undefined == $(this).data('locationWidget')) {
+            var plugin = new $.locationWidget(this, options);
+            $(this).data('locationWidget', plugin);
+        }
+    }
+})(jQuery)
